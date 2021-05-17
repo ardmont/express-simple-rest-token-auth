e82ee54f165e32faa434714f2c9641e91b9a145d
Ardiles montenegro <ardiles.montenegro@gmail.com>
2021-05-17 19:14:58 -0300

    Adds helmet.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const helmet = require('helmet')
+app.use(helmet())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
