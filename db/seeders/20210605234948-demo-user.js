acb412b340781f80748b4af8d63ea9b95c0c55c2
Ardiles montenegro <ardiles.montenegro@gmail.com>
2021-06-06 12:33:35 -0300

    Add basic migrations scaffold.

---
+++ db/seeders/20210605234948-demo-user.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      name: 'John',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+}
